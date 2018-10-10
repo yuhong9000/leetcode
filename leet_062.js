951a9af9277befa7d52b97772a90a052a2f23ef9
Yuhong Hu <yuhong9000@gmail.com>
2018-10-10 16:50:36 -0700

    Unique-Paths

---
+++ leet_062.js
@@ -0,0 +1,44 @@
+/**
+https://leetcode.com/problems/unique-paths/description/
+62. Unique Paths
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+
+Above is a 7 x 3 grid. How many possible unique paths are there?
+
+Note: m and n will be at most 100.
+
+Example 1:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+Example 2:
+
+Input: m = 7, n = 3
+Output: 28
+*/
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    const dp = new Array(n).fill(new Array(m).fill(1));
+
+    for(let i = 1; i < n; i++){
+        for(let j = 1; j < m; j++){
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        }
+    }
+
+    return dp[n-1][m-1];
+};
