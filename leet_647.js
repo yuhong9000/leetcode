84fd8e7f5d5d7d664662950c1e81a3aa11cbbcaf
Yuhong Hu <yuhong9000@gmail.com>
2018-10-11 19:50:05 -0700

    Palindromic Substrings

---
+++ leet_647.js
@@ -0,0 +1,40 @@
+/**
+https://leetcode.com/problems/palindromic-substrings/description/
+647. Palindromic Substrings
+Given a string, your task is to count how many palindromic substrings in this string.
+
+The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+
+Example 1:
+Input: "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+Input: "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+Note:
+The input string length won't exceed 1000.
+*/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function(s) {
+    let count = 0;
+    for(let i = 0; i < s.length; i++){
+        count += verifyPalindromeHelper(s,i,i);
+        count += verifyPalindromeHelper(s,i,i+1);
+    }
+    return count;
+};
+
+var verifyPalindromeHelper = function(s,left,right){
+    let count = 0;
+    while(left >= 0 && right < s.length && s[left] === s[right]){
+        count++;
+        left--;
+        right++;
+    }
+    return count;
+}
