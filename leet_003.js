94db4ea6f4282bbd496f4286586d798b9512ca73
Yuhong Hu <yuhong9000@gmail.com>
2018-10-22 15:40:43 -0700

    Longest-Substring-Without-Repeating-Characters

--- leet_003.js
+++ leet_003.js
@@ -1,29 +1,50 @@
 /**
 https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 3. Longest Substring Without Repeating Characters
+Given a string, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+Input: "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
 /**
  * @param {string} s
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let max = 0;
-    let pre = 0;
-    let len = 0;
-    const map = {};
+    const hash = {};
+
+    let start = 0; // starting index for substring
+    let max = 0; // max substring length
+    let len = 0; // current substring length
+
     for(let i = 0; i < s.length; i++){
-        if(s[i] in map){
-            max = Math.max(max,len);
-            len = len - (map[s[i]] - pre);
-            for(let j = pre; j < map[s[i]]; j++){
-                delete map[s[j]];
-            }
-            pre = map[s[i]] + 1;
-            map[s[i]] = i;
-        }else{
+        const index = s[i];
+
+        if(!(index in hash)|| hash[index] < start){
+            hash[index] = i;
             len++;
-            map[s[i]] = i;
         }
+        else{
+            start = hash[index] + 1;
+            hash[index] = i;
+            len = i - start + 1;
+        }
+        max = Math.max(len,max);
     }
-    return Math.max(max,len);
+
+    return max;
 };
